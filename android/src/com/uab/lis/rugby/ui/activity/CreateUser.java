8c4b19b62c0d29895a273edd51f80a3bb28aeba2
adria <adriaberge@gmail.com>
2014-03-30 16:12:17 +0200

    menus activitys

---
+++ android/src/com/uab/lis/rugby/ui/activity/CreateUser.java
@@ -0,0 +1,14 @@
+package com.uab.lis.rugby.ui.activity;
+
+import android.app.Activity;
+import android.os.Bundle;import com.uab.lis.rugby.R;
+
+/**
+ * Created by adria on 30/03/14.
+ */
+public class CreateUser extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_user);
+    }
+}
