dbdfee154634c63ea299255536943dfac33d7d14
adria <adriaberge@gmail.com>
2014-04-03 11:02:35 +0200

    menu principal

--- android/src/com/uab/lis/rugby/ui/activity/MenuPrincipal.java
+++ android/src/com/uab/lis/rugby/ui/activity/MenuPrincipal.java
@@ -9,11 +9,12 @@ import com.uab.lis.rugby.R;
  * Created by adria on 30/03/14.
  */
 public class MenuPrincipal extends Activity {
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meny_principal);
         Button jugar = (Button)findViewById(R.id.btnJugar);
         Button tienda = (Button)findViewById(R.id.btnTienda);
-        Button configurar = (Button)findViewById(R.id.btnConfigurar);
+        Button configuracion = (Button)findViewById(R.id.btnConfigurar);
     }
 }
